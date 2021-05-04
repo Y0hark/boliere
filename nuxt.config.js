9b79974aad6cc1fb4fab4488b35c3a85fa28f6b1
Samuel <samuelgaliere@gmail.com>
2021-05-04 20:53:42 +0200

    Fixed Babel bug that was showing a warning at server lauch

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,5 +51,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }]
+      ]
+    }
   }
 }
